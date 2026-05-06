4bbf81e5c426d8822c7cbddc727ab82d7587c577
Parisa Mapar <parisa.mapar@gmail.com>
2026-05-06 20:11:41 +0200

    Add files via upload

---
+++ src/load_data.py
@@ -0,0 +1,41 @@
+# Author: Parisa Mapar
+
+import pandas as pd
+import logging
+
+def load_data(file_path):
+
+    """
+    Load the input feature table from a CSV file.
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the input CSV file.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Loaded input data.
+
+    Raises
+    ------
+    Exception
+        Re-raises any exception encountered while reading the file.
+    """
+    
+    try:
+        # Attempt to read the Excel file into a pandas DataFrame.
+        data = pd.read_csv(file_path)
+        
+        # Return the loaded data if successful.
+        return data
+        
+    except Exception as e:
+    
+        # Log an error if there is an issue reading the file and re-raise the exception.
+        logging.error(f"Error in reading the input file: {e}")
+        raise
+    
+
+
